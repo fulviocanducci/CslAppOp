585cb6421b641da97eaa11412d517bf25ecde5b1
Fúlvio <fulviocanducci@hotmail.com>
2020-07-01 13:19:25 -0300

    Add project files.

---
+++ CslAppOperatorConvertion/Celsius.cs
@@ -0,0 +1,80 @@
+﻿namespace CslAppOperatorConvertion
+{
+    public class Celsius
+    {
+        public decimal Value { get; }
+
+        public Celsius(decimal value)
+        {
+            Value = value;
+        }
+
+        public static implicit operator Fahrenheit(Celsius celsius)
+        {
+            //°F = °C × 1.8 + 32;
+            decimal value = celsius.Value * 1.8m + 32;
+            return new Fahrenheit(value);
+        }
+    }
+
+    public class Fahrenheit
+    {
+        public decimal Value { get; }
+
+        public Fahrenheit(decimal value)
+        {
+            Value = value;
+        }
+
+        public static implicit operator Celsius(Fahrenheit fahrenheit)
+        {
+            //°C = (°F − 32) ÷ 1.8
+            decimal value = (fahrenheit.Value - 32) / 1.8m;
+            return new Celsius(value);
+        }
+    }
+
+    public class Sender
+    {
+        public double Value { get; }
+
+        public Sender(double value)
+        {
+            Value = value;
+        }
+
+        public static explicit operator int(Sender sender)
+        {
+            return (int)sender.Value;
+        }
+        public static explicit operator float(Sender sender)
+        {
+            return (float)sender.Value;
+        }
+
+    }
+
+    public class Source
+    {
+        public int Code { get; }
+        public Source(int code)
+        {
+            Code = code;
+        }
+
+        public static implicit operator bool(Source source)
+        {
+            return source.Code > 0;
+        }
+    }
+}
+
+        //public static Source operator +(Source s1, Source s2)
+        //{
+        //    return new Source(s1.Code + s2.Code);
+        //}
+
+        //public static Source operator -(Source s1, Source s2)
+        //{
+        //    return new Source(s1.Code + s2.Code);
+        //}
---
+++ CslAppOperatorConvertion/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data;
+
+namespace CslAppOperatorConvertion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Celsius c0 = new Celsius(0);
+            Fahrenheit f0 = c0;
+            Console.WriteLine(f0.Value);
+
+            Fahrenheit f1 = new Fahrenheit(32);
+            Celsius c1 = f1;
+            Console.WriteLine(c1.Value);
+
+            Sender s0 = new Sender(100.99d);
+            int i0 = (int)s0;
+            float fl0 = (float)s0;
+            Console.WriteLine(s0.Value);
+            Console.WriteLine(i0);
+            Console.WriteLine(fl0);
+
+            Source so0 = new Source(10);
+            if (so0) // code > 0
+            {
+                //
+            } 
+            else
+            {
+                //
+            }
+            Console.WriteLine(so0);
+        }
+    }
+}
